68eac8222876b5a9ebf1cd6599ed3bbc454f51d8
Britton Perry <britton.perry@hotmail.com>
2016-10-27 08:28:11 -0600

    Wrote the overriding init for the PlayingCard class

--- CardGame/untitled folder/PlayingCard.swift
+++ CardGame/untitled folder/PlayingCard.swift
@@ -22,16 +22,50 @@ class PlayingCard : Card
         }
     }
     
-    private var suit : String{
-        getBackImage(){
+    private var suit : String
+        {
+        get
+        {
             return self.suit
         }
         set(suit)
-            {
+        {
                 self.suit = suit
         }
     }
     
+    private var color : UIColor
+    {
+     get
+     {
+      return self.color
+     }
+        set(color)
+        {
+            self.color = color
+        }
+    }
+    
+    private var frontImage : UIImage
+    {
+        get
+        {
+            return self.frontImage
+        }
+        set(frontImage)
+        {
+            self.frontImage = frontImage
+        }
+    }
+    
+    override init()
+    {
+        super.init()
+        frontImage = UIImage()
+        color = UIColor()
+        rank = 0
+        suit = ""
+    }
     func getRank() -> Int
     {
         return self.rank
