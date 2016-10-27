1691c7967978ad9042eeb51e61d5d2d37625519e
Britton Perry <britton.perry@hotmail.com>
2016-10-27 08:18:55 -0600

    Updated get/set properties

--- CardGame/untitled folder/Card.swift
+++ CardGame/untitled folder/Card.swift
@@ -6,4 +6,47 @@
 //
 //
 
-import Foundation
+import UIKit
+class Card
+{
+    private var backImage : UIImage
+    {
+        get
+        {
+            return self.backImage
+        }
+        set(backImage)
+        {
+            self.backImage = backImage
+        }
+    }
+    
+    private var isFaceUp : Bool
+    
+    init()
+    {
+        isFaceUp = false
+        backImage = UIImage()
+    }
+    
+    func toString() -> String
+    {
+        let description = "The card is \(isFaceUp)"
+        return description
+    }
+    
+    func getBackImage() -> UIImage
+    {
+        return backImage
+    }
+    
+    func isUp() -> Bool
+    {
+       return isFaceUp
+    }
+    
+    func setIsFacing(isFaceUp : Bool) -> Void
+    {
+        self.isFaceUp = isFaceUp
+    }
+}
--- CardGame/untitled folder/PlayingCard.swift
+++ CardGame/untitled folder/PlayingCard.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class PlayingCard : Card
 {
+    private var rank : Int
+    {
+        get
+        {
+            return self.rank
+        }
+        set (rank)
+        {
+                self.rank = rank
+        }
+    }
     
+    private var suit : String{
+        getBackImage(){
+            return self.suit
+        }
+        set(suit)
+            {
+                self.suit = suit
+        }
+    }
+    
+    func getRank() -> Int
+    {
+        return self.rank
+    }
+    
+    func setRank(rank : Int)
+    {
+        self.rank = rank
+    }
 }
 
